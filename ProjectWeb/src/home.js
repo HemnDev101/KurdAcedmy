5f71153e9228b6a612b0293a8ce1871ea13da329
Hemn Ahmed <90180561+HemnDev101@users.noreply.github.com>
2022-01-31 20:02:48 +0300

    Version 1.1

---
+++ ProjectWeb/src/home.js
@@ -0,0 +1,37 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  // __________________________________ categries _________________________
+
+  function myFunctionc() {
+    document.getElementById("myDropdownc").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtnc')) {
+      var dropdowns = document.getElementsByClassName("dropdown-contentc");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
